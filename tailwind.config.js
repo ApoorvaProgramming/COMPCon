f8f70c3ed624e74ee33e3736a3d1e488963796d3
ApoorvaProgramming <135812642+ApoorvaProgramming@users.noreply.github.com>
2024-01-21 14:54:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html",   // Include HTML files
+    "./src/**/*.js",     // Include JavaScript files
+    "./src/**/*.css",    // Include CSS files
+    // Add more file types and paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // Add any additional plugins here
+  ],
+};
